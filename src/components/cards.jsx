077179e9911426d799dbbb133024278da847c62c
Guia <guia.sevilla.gs@gmail.com>
2025-12-12 17:09:58 +0100

    delete button

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -7,16 +7,23 @@ const [todos, setTodos] = useState([
   { id: new Date().getTime() + 3, list: "Pay bills", time: "4:30 PM" },
   { id: new Date().getTime() + 4, list: "Call a friend", time: "7:00 PM" },
   ]);
+  const deleteCard = (todoId) => {
+    setTodos(card => card.filter((todo) => {
+      console.log(todo.id, "!==", todoId)
+      return todo.id !== todoId;
+    }))
+  }
   return (
     <div className="todos-container">
+      
       {todos.map((todo) => (
         <div key={todo.id} className="todo-card">
           <p><strong>{todo.list}</strong></p>
           <p>{todo.time}</p>
 
-          <button>
+          <button onClick={() => deleteCard(todo.id)}>
         del
-      </button>
+       </button>
         </div>
       ))}
 
